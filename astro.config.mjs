7891790c2b42e814db43a4dec72d92f5cca2472e
Victorlyn066 <victorlyn066@gmail.com>
2025-07-26 15:59:03 +1000

    Configure Astro for API routes with Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://victorlyn.dev',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
 });
